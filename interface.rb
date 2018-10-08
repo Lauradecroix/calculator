6d2ce9926c0909892b049b7fbaaea848202e638c
Charles Pernet <charlespernet@gmail.com>
2018-10-08 10:21:01 +0200

    debut exo

---
+++ interface.rb
@@ -0,0 +1,7 @@
+# Saluer l'utilisateur
+# Demander un nombre
+# Demander un autre nombre
+# Demander un operateur
+  # Calculer le resultat
+  # calcutate(first_num, second_num, op)
+# Afficher le resultat
